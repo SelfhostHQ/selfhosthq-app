2236574347ca7a97bba2c2b23be74eb9fef5e1c5
Mathews Joseph <45147979+MathewsJoseph25@users.noreply.github.com>
2024-04-30 15:24:57 +0530

    setup the initial user

---
+++ init.ts
@@ -0,0 +1,31 @@
+import {} from 'bun'
+import { prismaClient } from 'prisma/prisma.client'
+const readInput = async (cmd?: string, required?: boolean) => {
+  if (cmd) process.stdout.write(cmd)
+  for await (const line of console) {
+    if (required && !line) {
+      {
+        console.warn(`${cmd} is required`)
+        continue
+      }
+    }
+    return line
+  }
+}
+
+console.log("Let's setup a user on your SelfhostHQ instance!")
+
+const name = await readInput('Name: ', true)
+const email = await readInput('Email: ', true)
+const password = await readInput('Password: ', true)
+
+const hashedPassword = await Bun.password.hash(password!)
+
+//create a user
+await prismaClient.users.create({
+  data: {
+    name: name!,
+    password: hashedPassword,
+    email: email!,
+  },
+})
