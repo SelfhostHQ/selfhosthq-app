92f3e44e983e4751cbf99a61a3a1a761a850a622
Mathews Joseph <45147979+MathewsJoseph25@users.noreply.github.com>
2024-05-02 21:04:12 +0530

    .

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -20,31 +20,29 @@ export async function loader({ request }: LoaderFunctionArgs) {
 export default function Index() {
   const { projects } = useLoaderData<typeof loader>()
   return (
-    <div className="bg-secondary min-h-screen flex justify-center items-center">
-      <div className="w-full max-w-2xl">
-        <div className="flex justify-between">
-          <h2>Select a project</h2>
+    <div className="bg-secondary min-h-screen p-10">
+      <div className="flex justify-between">
+        <h2>Select a project</h2>
 
-          <Link to="/new-project" className="text-primary">
-            + Create a new project
-          </Link>
-        </div>
-        {projects.length == 0 ? (
-          <div className="text-center my-20">--No projects found--</div>
-        ) : (
-          <div className=" grid grid-cols-1 gap-4 my-10 lg:grid-cols-2">
-            {projects.map((project) => (
-              <Link key={project.id} to={`/${project.handle}`}>
-                <div className=" py-3 px-2 bg-white rounded-lg">
-                  <span className=" font-bold">{project.name}</span>
-                  <br />
-                  <span>{project.description}</span>
-                </div>
-              </Link>
-            ))}
-          </div>
-        )}
+        <Link to="/new-project" className="text-primary">
+          + Create a new project
+        </Link>
       </div>
+      {projects.length == 0 ? (
+        <div className="text-center my-20">--No projects found--</div>
+      ) : (
+        <div className=" grid grid-cols-1 gap-4 my-10 md:grid-cols-2 lg:grid-cols-3">
+          {projects.map((project) => (
+            <Link key={project.id} to={`/${project.handle}`}>
+              <div className=" py-3 px-2 bg-white rounded-lg">
+                <span className=" font-bold">{project.name}</span>
+                <br />
+                <span>{project.description}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
